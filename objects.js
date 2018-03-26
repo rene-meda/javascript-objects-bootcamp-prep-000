65612b115362a639dd637945d61fc8ab6d124058
Adolpho Noguera <adolph518@gmail.com>
2018-03-26 20:12:48 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,8 @@
 var playlist = {shakira:"twerk_twerk"}
 
 function updatePlaylist(obj, name, title){
-  return obj.name = title
+  obj.name = title
+  return obj
 }
 
 function removeFromPlaylist(obj, name){
